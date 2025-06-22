2d20bb469edbd080eb6438edc5e4c062a9b30e35
DaniSaborio <danisabo8713@gmail.com>
2025-06-21 19:57:17 -0600

    1

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('¡Desplegado automáticamente con GitHub Actions! 🚀');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
