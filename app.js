314625f1610c77cc0cf48601457e8ab147fea0ed
DaniSaborio <danisabo8713@gmail.com>
2025-06-21 20:21:47 -0600

    3

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
 
 //qq
+//8// Para ejecutar la aplicación, usa el comando:
+// node app.js
+// Asegúrate de tener Node.js y Express instalados en tu entorno.
+// Puedes instalar Express con el comando:
